a7b9f3edca52dd83e12d772e82ca74d44bb01e2a
Vadim Gulakov <gulakov.vadim2017@yandex.ru>
2020-12-09 15:10:35 +0300

    Чистка комментов

--- lab2/src/bsu/rfe/java/group8/lab2/Gulakov/varB6/MainFrame.java
+++ lab2/src/bsu/rfe/java/group8/lab2/Gulakov/varB6/MainFrame.java
@@ -24,27 +24,27 @@ public class MainFrame extends JFrame {
 private static final int WIDTH = 600; 
 private static final int HEIGHT = 420; 
 
-//Текстовые поля для считывания значений переменных, 
+
 private JTextField textFieldX; 
 private JTextField textFieldY; 
 private JTextField textFieldZ; 
 
 
 
-//Текстовое поле для отображения результата,  
+  
 private JTextField memoryTextField;
 private JTextField resultFieldText;
 
-//Группа радио-кнопок для обеспечения уникальности выделения в группе 
+
 private ButtonGroup radioButtons = new ButtonGroup(); 
 private ButtonGroup radioMemoryButtons = new ButtonGroup();
 
-//Контейнер для отображения радио-кнопок 
+
 private Box hboxFormulaType = Box.createHorizontalBox(); 
 private Box hBoxMemoryType = Box.createHorizontalBox();
 
 
-//Переменная, указывающая, какая из формул является  активной  в  данный  момент
+
 private int formulaId = 1;
 private int memoryId = 1;
 
@@ -109,16 +109,14 @@ private Double mem3 = new Double(0);
 				/ (z * Math.log(x + y));
 	}
 	
-	//Вспомогательный метод для добавления кнопок на панель
-	//buttonName – текст рядом с кнопкой, formulaId – идентификатор формулы 
+	
 	private void addRadioButton(String buttonName, final int formulaId) {
 
-		//Создать экземпляр радио-кнопки с заданным текстом 
+		
 		JRadioButton button = new JRadioButton(buttonName); 
-		//Добавить радио-кнопку в группу 
+	
 		radioButtons.add(button); 
-		//Добавить радио-кнопку в 
-		//Для этого ссылка на контейнер сделана полем данных класса 
+		
 		hboxFormulaType.add(button); 
 		} 
 
@@ -140,124 +138,108 @@ private Double mem3 = new Double(0);
 
 
 
-	//Конструктор класса 
+	
 	public MainFrame() { 
 	super("Вычисление формулы"); 
 	setSize(WIDTH, HEIGHT); 
 	Toolkit kit = Toolkit.getDefaultToolkit(); 
 	
-	// Отцентрировать окно приложения на экране 
+	
 	setLocation((kit.getScreenSize().width - WIDTH)/2, 
 	(kit.getScreenSize().height - HEIGHT)/2); 
 
 
-	//Добавить «клей» C1-H1 с левой стороны 
+	
 	hboxFormulaType.add(Box.createHorizontalGlue()); 
-	//Создать радио-кнопку для формулы 1 
+	
 	addRadioButton("Формула 1", 1); 
-	//Создать радио-кнопку для формулы 2 
+	
 	addRadioButton("Формула 2", 2); 
-	//Установить выделенной 1-ую кнопку из группы 
+	 
 	radioButtons.setSelected(radioButtons.getElements().nextElement().getModel(), true); 
-	//Добавить «клей» C1-H2 с правой стороны 
+	
 	hboxFormulaType.add(Box.createHorizontalGlue()); 
 	 
-	// Создать область с полями ввода для X и Y 
-	//Создать подпись “X:” для переменной X 
+	
 	JLabel labelForX = new JLabel("X:"); 
-	//Создать текстовое поле для ввода значения переменной X,   
-	//(по умолчанию 0) 
+	
 	textFieldX = new JTextField("0.0", 10); 
-	//Установить макс размер = желаемому для предотвращения масштабирования 
+	
 	textFieldX.setMaximumSize(textFieldX.getPreferredSize()); 
-	//Создать подпись “Y:” для переменной Y 
+	
 	JLabel labelForY = new JLabel("Y:"); 
-	//Создать текстовое поле для ввода значения переменной Y, 
-	//(по умолчанию 0) 
+	
 	textFieldY = new JTextField("0.0", 10); 
-	//Установить макс размер = желаемому для предотвращения масштабирования 
+	
 	textFieldY.setMaximumSize(textFieldY.getPreferredSize()); 
-	//Создать подпись “Z:” для переменной Z 
+	
 	JLabel labelForZ = new JLabel("Z:"); 
-	//Создать текстовое поле для ввода значения переменной Z, 
-	//(по умолчанию 0) 
+	
 	textFieldZ = new JTextField("0.0", 10); 
-	//Установить макс размер = желаемому для предотвращения масштабирования 
+	
 	textFieldZ.setMaximumSize(textFieldZ.getPreferredSize()); 
 
-	//Создать контейнер «коробка с горизонтальной укладкой» 
+	 
 	Box hboxVariables = Box.createHorizontalBox(); 
-	//Задать рамку для коробки с помощью класса BorderFactory 
-	//hboxVariables.setBorder(BorderFactory.createLineBorder(Color.RED));   
-	//Добавить в контейнер ряд объектов: 
-	//Добавить «клей» C2-H1 – для максимального удаления от левого края 
+	
 	hboxVariables.add(Box.createHorizontalGlue()); 
-	//Добавить подпись для переменной Х 
+	
 	hboxVariables.add(labelForX); 
-	//Добавить «распорку» C2-H2 шириной 10 пикселов для отступа между  
-	//надписью и текстовым полем для ввода значения X 
+	
 	hboxVariables.add(Box.createHorizontalStrut(10)); 
-	//Добавить само текстовое поле для ввода Х  
+	
 	hboxVariables.add(textFieldX); 
-	//Добавить «распорку» C2-H3 шириной 50 пикселов для отступа между  
-	//текстовым полем для ввода X и подписью для Y 
+	
 	hboxVariables.add(Box.createHorizontalStrut(50)); 
-	//Добавить подпись для переменной Y 
+	 
 	hboxVariables.add(labelForY); 
-	//Добавить «распорку» C2-H4 шириной 10 пикселов для отступа между  
-	//надписью и текстовым полем для ввода значения Y 
+	
 	hboxVariables.add(Box.createHorizontalStrut(10)); 
-	//Добавить само текстовое поле для ввода Y 
+	
 	hboxVariables.add(textFieldY);
-	//Добавить «распорку» C2-H5 шириной 50 пикселов для отступа между  
-	//текстовым полем для ввода Y и подписью для Z
+	
 	hboxVariables.add(Box.createHorizontalStrut(50)); 
-	//Добавить подпись для переменной Z
+	
 	hboxVariables.add(labelForZ);
-	//Добавить «распорку» C2-H6 шириной 10 пикселов для отступа между  
-	//надписью и текстовым полем для ввода значения Z 
+	
 	hboxVariables.add(Box.createHorizontalStrut(10)); 
-	//Добавить текстовое поле для ввода Z 
+	
 	hboxVariables.add(textFieldZ); 
-	//Добавить «клей» C2-H5 для максимального удаления от правого края 
+	 
 	hboxVariables.add(Box.createHorizontalGlue());
 
 
 
-	//Создать подпись для поля с результатом 
+	
 	JLabel labelForResult = new JLabel("Результат:"); 
-	// Создать текстовое поле для вывода результата, начальное значение - 0 
+	
 	resultFieldText = new JTextField("0", 15); 
-	//Установить макс размер = желаемому для предотвращения масштабирования 
+	
 	resultFieldText.setMaximumSize( resultFieldText.getPreferredSize()); 
 
 
-	// Создать контейнер «коробка с горизонтальной укладкой» 
+	
 	Box hboxResult = Box.createHorizontalBox(); 
-	// Добавить в контейнер ряд объектов 
-	// Добавить «клей» C3-H1 для отступа от левого края 
+	
 	hboxResult.add(Box.createHorizontalGlue()); 
-	//Добавить подпись для результата 
+	
 	hboxResult.add(labelForResult); 
-	//Добавить «распорку» C3-H2 в 10 пикселов между подписью и полем результата 
+	 
 	hboxResult.add(Box.createHorizontalStrut(10)); 
-	//Добавить текстовое поле для вывода результата 
+	
 	hboxResult.add(resultFieldText); 
-	//Добавить «клей» C3-H3 справа 
+	
 	hboxResult.add(Box.createHorizontalGlue()); 
-	//Задать рамку для контейнера 
+	
 	hboxResult.setBorder(BorderFactory.createLineBorder(Color.RED)); 
 
 
-	// Создать область для кнопок 
-	// Создать кнопку «Вычислить» 
+	
 	JButton buttonCalc = new JButton("Вычислить"); 
-	// Определить и зарегистрировать обработчик нажатия на кнопку 
+	 
 	buttonCalc.addActionListener(new ActionListener() { 
 		 public void actionPerformed(ActionEvent ev) { 
-			 //Преобразование введенных строк в числа с плавающей точкой может  
-			 //спровоцировать исключительную ситуацию при неправильном формате чисел, 
-			 //поэтому необходим блок try-catch 
+			  
 			 try { 
 				 
 				 Double x = Double.parseDouble(textFieldX.getText()); 
@@ -284,11 +266,11 @@ private Double mem3 = new Double(0);
 		addMemoryRadioButton("Память 1", 1);
 		addMemoryRadioButton("Память 2", 2);
 		addMemoryRadioButton("Память 3", 3);
-		// по умолчанию на первую память
+		
 		radioMemoryButtons.setSelected(radioMemoryButtons.getElements().nextElement().getModel(), true); 
-		//Добавить «клей» C1-H2 с правой стороны 
+		 
 		hBoxMemoryType.add(Box.createHorizontalGlue()); 
-		//Задать рамку для коробки с помощью класса BorderFactory 
+		 
 		hBoxMemoryType.setBorder(BorderFactory.createLineBorder(Color.YELLOW)); 
 
 	
@@ -350,44 +332,43 @@ private Double mem3 = new Double(0);
 		hBoxControlButtons.add(buttonMp);
 		hBoxControlButtons.add(Box.createHorizontalGlue());
 		 
-	//Создать коробку с горизонтальной укладкой 
+	
 	Box hboxButtons = Box.createHorizontalBox(); 
-	//Добавить «клей» C4-H1 с левой стороны 
+	
 	hboxButtons.add(Box.createHorizontalGlue()); 
-	//Добавить кнопку «Вычислить» в компоновку 
+	
 	hboxButtons.add(buttonCalc); 
-	//Добавить распорку в 30 пикселов C4-H2 между кнопками 
+	 
 	hboxButtons.add(Box.createHorizontalStrut(30)); 
-	//Добавить кнопку «Очистить поля» в компоновку 
+	 
 	hboxButtons.add(buttonReset); 
-	//Добавить «клей» C4-H3 с правой стороны 
+	
 	hboxButtons.add(Box.createHorizontalGlue()); 
-	//Задать рамку для контейнера 
+	 
 	hboxButtons.setBorder(BorderFactory.createLineBorder(Color.GREEN)); 
 
 
-	// Связать области воедино в компоновке BoxLayout 
-	//Создать контейнер «коробка с вертикальной укладкой» 
+	
 	Box contentBox = Box.createVerticalBox(); 
-	//Добавить «клей» V1 сверху 
+	 
 	contentBox.add(Box.createVerticalGlue()); 
-	// Добавить контейнер с выбором формулы 
+	
 	contentBox.add(hboxFormulaType); 
-	//Добавить контейнер с переменными 
+	 
 	contentBox.add(hboxVariables); 
-	//Добавить контейнер с результатом вычислений 
+	 
 	contentBox.add(hboxResult); 
-	//Добавить контейнер с кнопками 
+	
 	contentBox.add(hboxButtons); 
-	// Добавить контейнер с выбором памяти
+	
 	contentBox.add(hBoxMemoryType); 
-	//Добавить резултат 
+	
 	contentBox.add(hBoxControlButtons); 
-	/// поле
+	
 	contentBox.add(hBoxMemoryField); 
-	//Добавить «клей» V2 снизу 
+	
 	contentBox.add(Box.createVerticalGlue());    
-	//Установить «вертикальную коробку» в область содержания главного окна
+	
 	getContentPane().add(contentBox, BorderLayout.CENTER); 
 
 	} 
